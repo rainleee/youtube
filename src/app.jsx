73c9ad42b3b5ecf385951dee6bd1661ab9818e8f
rainlee4401 <rainlee4401@gmail.com>
2021-07-15 16:56:30 +0900

    config name 변경

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './app.css';
-import config from './config.js';
+import CONFIG from './config.js';
 import Navbar from './component/video_search/navbar';
 import VideoDetail from './component/video_detail/video_detail';
 import VideoList from './component/video_list/video_list';
@@ -9,14 +9,13 @@ function App() {
   const [videos, setVideos] = useState([]);
   const [selectVideo, setSelectVideo] = useState(null);
   const [searchVideo, setSearchVideos] = useState(null);
-  const [reqHttps, setReqHttps] = useState(`https://youtube.googleapis.com/youtube/v3/videos?key=${config.API_KEY}&part=snippet&part=statistics&chart=mostPopular&maxResults=25&regionCode=KR`);
+  const [reqHttps, setReqHttps] = useState(`https://youtube.googleapis.com/youtube/v3/videos?key=${CONFIG.API_KEY}&part=snippet&part=statistics&chart=mostPopular&maxResults=25&regionCode=KR`);
   const handleVideoDetail = video => {
     setSelectVideo(video);
   }
-
   //search videos
   const handleSearhVideos = searchVal => {
-    setReqHttps(`https://youtube.googleapis.com/youtube/v3/search?key=${config.API_KEY}&part=snippet&maxResult=3&q=${searchVal}&nextPageToken=CBkQAA`);
+    setReqHttps(`https://youtube.googleapis.com/youtube/v3/search?key=${CONFIG.API_KEY}&part=snippet&maxResult=3&q=${searchVal}&nextPageToken=CBkQAA`);
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
