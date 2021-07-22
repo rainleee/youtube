d6ecd05405aa6b826c466ef8ca4c4630c489895e
rainlee4401 <rainlee4401@gmail.com>
2021-07-22 13:38:28 +0900

    app.jsx config https 주소 위치 변경

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,35 @@ import VideoList from './component/video_list/video_list';
 function App() {
   const [videos, setVideos] = useState([]);
   const [selectVideo, setSelectVideo] = useState(null);
-  let reqHttps = `https://youtube.googleapis.com/youtube/v3/videos?key=${CONFIG.API_KEY}&part=snippet&part=statistics&chart=mostPopular&maxResults=25&regionCode=KR`;
+
+  //Fav videos fetch
+  const fetchReqFavVideos = (reqHttps, requestOptions) => {
+    fetch(reqHttps, requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }
+
+  //init or update 
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetchReqFavVideos(CONFIG.HTTPS_ADDR, requestOptions);
+  }, []);
+
+  //logo click event
+  const handleMainPage = () => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetchReqFavVideos(CONFIG.HTTPS_ADDR, requestOptions);
+  }
+
 
   const handleVideoDetail = video => {
     setSelectVideo(video);
@@ -16,7 +44,7 @@ function App() {
 
   //search videos
   const handleSearhVideos = query => {
-    reqHttps = `https://youtube.googleapis.com/youtube/v3/search?key=${CONFIG.API_KEY}&type=video&part=snippet&maxResults=25&q=${query}&nextPageToken=CBkQAA`;
+    const reqHttps = `https://youtube.googleapis.com/youtube/v3/search?key=${CONFIG.API_KEY}&type=video&part=snippet&maxResults=25&q=${query}&nextPageToken=CBkQAA`;
 
     const requestOptions = {
       method: 'GET',
@@ -32,24 +60,20 @@ function App() {
       .catch(error => console.log('error', error))
   }
 
-  useEffect(() => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(reqHttps, requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
-
   return (
     <div className={styles.app}>
-      <SearchHeader onSearchVideo={handleSearhVideos} />
+      <SearchHeader onSearchVideo={handleSearhVideos} onMainPage={handleMainPage} />
       <VideoList videos={videos} />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+  해야할 것 목록
+  1. fetch이벤트를 효율적으로 처리하지 못하고 계속 이벤트가 생성될떄마다 부르는 것
+  2.reqHttps를 useState로 할 시 디펜던시 관련 에러가 나와서 변수로 처리중인데 그부분을 고칠것
+
+*/
--- src/component/search_header/search_header.jsx
+++ src/component/search_header/search_header.jsx
@@ -1,27 +1,39 @@
 import React, { useRef } from 'react';
 import styles from './search_header.module.css';
 
-const SearchHeader = ({ onSearchVideo }) => {
+const SearchHeader = ({ onSearchVideo, onMainPage }) => {
     const searchInput = useRef();
 
-
+    //search logic event
     const onSearchEvent = () => {
         const searchVal = searchInput.current.value;
         onSearchVideo(searchVal);
     }
 
+    //search Enter event
     const onKeyPress = event => {
         if (event.key === 'Enter') onSearchEvent();
     }
 
+    //search button click event
+    const onClickSearch = () => {
+        onSearchEvent();
+    }
+
+    //logoClick event
+    const onClickMainPage = () => {
+        onMainPage();
+        searchInput.current.value = "";
+    }
+
     return (
         <header className={styles.header}>
-            <div className={styles.logo} >
+            <div className={styles.logo} onClick={onClickMainPage}>
                 <img className={styles.img} src="/images/logo.png" alt="logo" />
                 <h1 className={styles.title}>Youtube</h1>
             </div>
             <input className={styles.input} type="text" placeholder="검색" ref={searchInput} onKeyPress={onKeyPress} />
-            <button className={styles.button} type="submit">
+            <button className={styles.button} type="submit" onClick={onClickSearch}>
                 <i className="fas fa-search"></i>
             </button>
         </header>
