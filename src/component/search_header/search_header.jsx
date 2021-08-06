b7e6e6931d983ed4597e8648fd97da3a730a8cdc
rainlee <rainlee4401@gmail.com>
2021-08-06 17:51:02 +0900

    search-bar 수정중

--- src/component/search_header/search_header.jsx
+++ src/component/search_header/search_header.jsx
@@ -26,24 +26,33 @@ const SearchHeader = memo(({ onSearchVideo, onMainPage }) => {
     searchInput.current.value = "";
   };
 
-  console.log("search Header!!!");
-
   return (
     <header className={styles.header}>
-      <div className={styles.logo} onClick={onClickMainPage}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
-        <h1 className={styles.title}>Youtube</h1>
+      <div className={styles.container}>
+        <div className={styles.logo} onClick={onClickMainPage}>
+          <img className={styles.img} src="/images/logo.png" alt="logo" />
+          <h1 className={styles.title}>Youtube</h1>
+        </div>
+        <div className={styles.search}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="검색"
+            ref={searchInput}
+            onKeyPress={onKeyPress}
+          />
+          <button
+            className={styles.button}
+            type="submit"
+            onClick={onClickSearch}
+          >
+            <i className={`fas fa-search ${styles.searchBtn}`}></i>
+          </button>
+        </div>
+        <div className={styles.profile}>
+          <i className={`fas fa-user-circle ${styles.user}`}></i>
+        </div>
       </div>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="검색"
-        ref={searchInput}
-        onKeyPress={onKeyPress}
-      />
-      <button className={styles.button} type="submit" onClick={onClickSearch}>
-        <i className="fas fa-search"></i>
-      </button>
     </header>
   );
 });
