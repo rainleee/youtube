abe45a0da905a7f75b0b53f0985a02a199642ea4
rainlee <rainlee4401@gmail.com>
2021-08-09 19:22:02 +0900

    searchHeader CSS 레이아웃 수정

--- src/component/search_header/search_header.jsx
+++ src/component/search_header/search_header.jsx
@@ -28,10 +28,13 @@ const SearchHeader = memo(({ onSearchVideo, onMainPage }) => {
 
   return (
     <header className={styles.header}>
+      <div className={styles.menu}>
+        <i class={`fas fa-bars ${styles.menubar}`}></i>
+      </div>
       <div className={styles.container}>
         <div className={styles.logo} onClick={onClickMainPage}>
-          <img className={styles.img} src="/images/logo.png" alt="logo" />
-          <h1 className={styles.title}>Youtube</h1>
+          <img className={styles.logoImg} src="/images/logo.png" alt="logo" />
+          <h1 className={styles.title}>YouTube</h1>
         </div>
         <div className={styles.search}>
           <input
@@ -49,8 +52,9 @@ const SearchHeader = memo(({ onSearchVideo, onMainPage }) => {
             <i className={`fas fa-search ${styles.searchBtn}`}></i>
           </button>
         </div>
-        <div className={styles.profile}>
-          <i className={`fas fa-user-circle ${styles.user}`}></i>
+        <div className={styles.information}>
+          <i class={`fas fa-bell ${styles.notice}`}></i>
+          <i className={`fas fa-user-circle ${styles.profile}`}></i>
         </div>
       </div>
     </header>
