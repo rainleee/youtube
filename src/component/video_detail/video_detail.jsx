b9544401545da7275d650ed6f62fa668222641a8
rainlee <rainlee4401@gmail.com>
2021-08-11 15:44:49 +0900

    video Detail PC버전 CSS 수정

--- src/component/video_detail/video_detail.jsx
+++ src/component/video_detail/video_detail.jsx
@@ -11,24 +11,26 @@ const VideoDetail = ({
   const date = new Date(snippet.publishedAt);
   return (
     <section className={styles.detail}>
-      <iframe
-        className={styles.videoPlayer}
-        type="text/html"
-        title="youtube video player"
-        width="100%"
-        height="500"
-        src={`https://www.youtube.com/embed/${id}`}
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      <h2>{snippet.title}</h2>
+      <div className={styles.iframebox}>
+        <iframe
+          className={styles.videoPlayer}
+          type="text/html"
+          title="youtube video player"
+          src={`https://www.youtube.com/embed/${id}`}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <h2 className={styles.title}>{snippet.title}</h2>
       {/* <h2>조회수 {viewCount}회</h2> */}
-      <h3>{date.toLocaleDateString()}</h3>
+      <h3 className={styles.uploadDate}>{date.toLocaleDateString()}</h3>
       {/* <h2>좋아요 {statistics.likeCount}</h2>
             <h2>싫어요 {statistics.dislikeCount}</h2> */}
-      <h3>{snippet.channelTitle}</h3>
+      <h3 className={styles.channelTitle}>채널명 : {snippet.channelTitle}</h3>
+      <h4 className={styles.subcribCount}>
+        구독자 {parseInt(subcribCount).toLocaleString("ko-KR")}명
+      </h4>
       <pre className={styles.description}>{snippet.description}</pre>
-      <h4>구독자 {parseInt(subcribCount).toLocaleString("ko-KR")}명</h4>
       {/* <h2>tags : {snippet.tags.map(tag => <span>#{tag} </span>)}</h2> */}
     </section>
   );
