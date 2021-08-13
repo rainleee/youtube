04f74f9b0472050682a4ad2d243528fadd3c254b
rainlee <rainlee4401@gmail.com>
2021-08-13 18:19:37 +0900

    CSS refactoring Detail 일부완료

--- src/component/video_detail/video_detail.jsx
+++ src/component/video_detail/video_detail.jsx
@@ -10,7 +10,7 @@ const VideoDetail = ({
   // const viewCount = parseInt(statistics.viewCount).toLocaleString('ko-KR');
   const date = new Date(snippet.publishedAt);
   return (
-    <section className={styles.detail}>
+    <div className={styles.container}>
       <div className={styles.iframebox}>
         <iframe
           className={styles.videoPlayer}
@@ -21,19 +21,25 @@ const VideoDetail = ({
           allowFullScreen
         ></iframe>
       </div>
-      <h2 className={styles.title}>{snippet.title}</h2>
-      <h3 className={styles.uploadDate}>{date.toLocaleDateString()}</h3>
-      <h3 className={styles.channelTitle}>채널명 : {snippet.channelTitle}</h3>
-      <h4 className={styles.subcribCount}>
-        구독자 {parseInt(subcribCount).toLocaleString("ko-KR")}명
-      </h4>
-      <pre className={styles.description}>{snippet.description}</pre>
-      {/*   search API 호출 시 해당 데이터를 불러올 수 없어 보류  
+      <div className={styles.info}>
+        <h1 className={styles.title}>{snippet.title}</h1>
+        <h3 className={styles.upload__date}>
+          게시일: {date.toLocaleDateString()}
+        </h3>
+        <div className={styles.channel__info}>
+          <h2 className={styles.channel__title}>{snippet.channelTitle}</h2>
+          <h3 className={styles.subcrib__count}>
+            구독자 {parseInt(subcribCount).toLocaleString("ko-KR")}명
+          </h3>
+        </div>
+        <pre className={styles.description}>{snippet.description}</pre>
+        {/*   search API 호출 시 해당 데이터를 불러올 수 없어 보류  
         <h2>좋아요 {statistics.likeCount}</h2>
             <h2>싫어요 {statistics.dislikeCount}</h2> */}
-      {/* <h2>조회수 {viewCount}회</h2> */}
-      {/* <h2>tags : {snippet.tags.map(tag => <span>#{tag} </span>)}</h2> */}
-    </section>
+        {/* <h2>조회수 {viewCount}회</h2> */}
+        {/* <h2>tags : {snippet.tags.map(tag => <span>#{tag} </span>)}</h2> */}
+      </div>
+    </div>
   );
 };
 
